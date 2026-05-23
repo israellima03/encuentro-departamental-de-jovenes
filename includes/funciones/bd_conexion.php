b4299a2f41864bf5cee1f1eae389340ea867ba2e
isra <israellimacondori32@gmail.com>
2026-05-23 18:08:55 -0400

    me olvide cambiar la ruta de local al servidor xd

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,6 +1,6 @@
 <?php
-$conn = new mysqli('localhost', 'root', 'root', 'encuentro_departamental', 8889);
+$conn = new mysqli('localhost','u326069131_user','Lim@882003','u326069131_encuentro');
 if($conn->connect_error){
   echo $conn->connect_error;
 }
-?>   
+?>
